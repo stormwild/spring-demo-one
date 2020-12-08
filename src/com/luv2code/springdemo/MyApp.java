c2804d8d406e89c76a03c95265e8de553dd3502e
stormwild <artorrijos@gmail.com>
2020-12-08 11:21:14 +0000

    Added theia debugging configuration for main class

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -2,13 +2,12 @@ package com.luv2code.springdemo;
 
 public class MyApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// create object
-		ICoach coach = new TrackCoach();
-		
-		// use object
-		System.out.println(coach.getDailyWorkout());
-	}
+    public static void main(String[] args) {
+        // create object
+        ICoach coach = new TrackCoach();
+
+        // use object
+        System.out.println(coach.getDailyWorkout());
+    }
 
 }
